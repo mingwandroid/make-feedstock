c9ca058f486631232fcda50dde3b448aa31a93ce
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-08-09 20:18:29 -0400

    Install gnumake as make too
    Should make it a little easier for users to work with. Especially if
    they weren't aware of the `gnu` prefix.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,3 +7,5 @@ if errorlevel 1 exit 1
 copy .\\GccRel\\gnumake.exe  %LIBRARY_BIN%\\gnumake.exe
 if errorlevel 1 exit 1
 
+copy .\\GccRel\\gnumake.exe  %LIBRARY_BIN%\\make.exe
+if errorlevel 1 exit 1
