9c34067c1777b394b446d54fb3ed68d8f6d6b687
Anthony Scopatz <scopatz@gmail.com>
2020-02-24 21:24:40 -0500

    config opts

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,13 @@ if [[ "$(uname)" == 'Linux' ]]; then
   rm -f tests/scripts/functions/wildcard
 fi
 
-./configure --prefix=$PREFIX
+if [[ ${target_platform} =~ .*aarch64.* || ${target_platform} =~ .*ppc64le.* ]]; then
+  config_opts="--disable-dependency-tracking"
+else
+  config_opts=""
+fi
+
+./configure --prefix=$PREFIX ${config_opts}
 # bootstrap building make without make
 bash build.sh
 # make
