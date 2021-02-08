a7906cea6717442215b619e22d95ac342e450e76
Ilan Schnell <ilanschnell@gmail.com>
2021-02-08 16:58:27 -0600

    fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Get an updated config.sub and config.guess
-if [[ ! $CONDA_BUILD_BOOTSTRAPPING == yes ]]; then
+if [[ ! $BOOTSTRAPPING == yes ]]; then
   cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build-aux
 fi
 set -ex
